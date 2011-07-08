bede8989eb9386bf2a9193bcf1736a2e5e820abb
Vineet Naik <vineet.naik@kodeplay.com>
2011-07-08 18:51:27 +0530

    actions added to Auth Controller

--- application/classes/controller/auth.php
+++ application/classes/controller/auth.php
@@ -3,19 +3,31 @@
 class Controller_Auth extends Controller {
 
     public function action_login() {
-     
+        $form = $this->form_login();
+        var_dump($form);
+        exit;
     }
 
-    private function login_form() {
-
+    private function form_login() {
+        $action = 'auth/login';
+        $form = new Stickyform($action);
+        $form->default_data = array(
+            'email' => '',
+            'password' => '',
+        );
+        $form->posted_data = $this->request->post();
+        $form->append('Email', 'email', 'text');
+        $form->append('Password', 'password', 'password');
+        $form->process();
+        return $form;
     }
 
     public function action_register() {
-
+        
     }
 
-    private function register_form() {
-
+    private function form_register() {
+        
     }
 
     public function action_logout() {
