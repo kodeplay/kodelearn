c88eb8d364cc08a67c64d1916b90b2a3b0215157
Nikhil Tailang <nikhil@kodeplay.com>
2011-07-08 16:16:29 +0100

    Added batch, sidebar and made changes to the css

---
+++ modules/playground/views/play/batchCreate.php
@@ -0,0 +1,49 @@
+<?php include_once 'header.php'; ?>
+
+
+	<?php include_once 'sidebar.php'; ?>
+	
+	<div class="r pagecontent">
+		<div class="pageTop withBorder">
+			<div class="pageTitle l">Create a batches</div>
+			<div class="pageDesc r">this is a test description this is a test description this is a test description this is a test description this is a test description </div>
+			<div class="clear"></div>
+		</div><!-- pageTop -->
+		
+		<table class="formcontainer bm40">
+			<tr>
+				<td><label for="cbName">Name</label></td>
+				<td><input type="text" id="cbName" /></td>
+			</tr>
+			<tr>
+				<td class="vatop"><label for="cbDesc">Description</label></td>
+				<td><textarea id="cbDesc" rows="5"></textarea></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td>
+					<span class="l">
+						<a href="#" class="button">Save</a>
+					</span>
+					<span class="r">
+						<a href="#">or cancel</a>
+					</span>
+				</td>
+			</tr>
+		</table>
+		
+		<p class="sectionTitle">Add students to this batch</p>
+		<p class="vm10">A CSV file is the easiest way to add students to a batch. ABCDEFGHIJKLMNOPQRSTUVWXYZ.</p>
+		<p class="tm40">
+			<a href="#" class="button">Upload now</a>
+		</p>
+		<p class="vm40"><a href="#">or click here to download a sample CSV file</a></p>
+		
+		
+		
+	</div><!-- content -->
+	
+	<div class="clear"></div>
+	
+
+<?php include_once 'footer.php'; ?>
