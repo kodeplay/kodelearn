cb9b34c479e0d35a5dc22d8e6b69b8b2713a5a71
Vineet Naik <vineet.naik@kodeplay.com>
2011-07-08 17:58:52 +0530

    enabled required modules (db, orm, auth), added auth controller

---
+++ application/classes/controller/auth.php
@@ -0,0 +1,28 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Auth extends Controller {
+
+    public function action_login() {
+     
+    }
+
+    private function login_form() {
+
+    }
+
+    public function action_register() {
+
+    }
+
+    private function register_form() {
+
+    }
+
+    public function action_logout() {
+
+    }
+
+    public function action_recoverpass() {
+
+    }
+}
