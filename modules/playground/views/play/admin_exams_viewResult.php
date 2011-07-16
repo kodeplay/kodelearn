d12db9c9836de9781f50c8e12d285c63f02a7862
Vineet Naik <vineet.naik@kodeplay.com>
2011-07-16 12:19:04 +0530

    merged conflicts

---
+++ modules/playground/views/play/admin_exams_viewResult.php
@@ -0,0 +1,192 @@
+<?php include_once 'header.php'; ?>
+
+
+	<?php include_once 'sidebar.php'; ?>
+	
+	<div class="r pagecontent">
+	<!-- Exam Result | prefix: exr--> 
+		<div class="pageTop">
+			<div class="pageTitle l">Results</div>
+			<div class="pageDesc r">You can add results for exams here.</div>
+			<div class="clear"></div>
+		</div><!-- pageTop -->
+		
+		<div class="topbar">
+			<h2 class="h2 tac">
+				<span class=""><a href="#">Previous</a></span>
+				Student: <span id="exrStuName">Shinchan</span>. 
+				Course: <span id="exrCourse">MBA (IT)</span>
+				<span><a href="#">Next</a></span>
+			</h2>
+			<select name="" id="" class="r hidden">
+				<option value="">Test 1</option>
+				<option value="">Test 2</option>
+				<option value="">Test 3</option>
+			</select>
+		</div><!-- topbar -->
+		
+		<table class="datatable fullwidth bm40">
+			<tr>
+				<th><input type="checkbox" /></th>
+				<th>Exam</th>
+				<th>Batch/ Course</th>
+				<th>Grading period</th>
+				<th>Schedule</th>
+				<th>Send reminder</th>
+				<th>Actions</th>
+			</tr>
+			<tr>
+				<td><input type="checkbox" /></td>
+				<td>Maths M01</td>
+				<td>
+					<p>Standard 8A</p>
+					<p>Maths 8A</p>
+				</td>
+				<td>First Semester</td>
+				<td>
+					<p>22-01-2011</p>
+					<p>10AM to 12PM</p>
+					<p>Room 404</p>
+					<p>Old Building</p>
+				</td>
+				<td>
+					<div class="toggleButton">
+						<a href="#" class="l on">On</a>
+						<a href="#" class="l">Off</a>
+						<span class="clear"></span>
+					</div>
+					
+				</td>
+				<td>
+					<p><a href="#">View</a>/ <a href="#">Edit</a></p>
+					<p><a href="#">Enter result</a></p>
+					<p><a class="tRed" href="#">Delete</a></p>
+				</td>
+			</tr>
+			<tr>
+				<td><input type="checkbox" /></td>
+				<td>Maths M01</td>
+				<td>
+					<p>Standard 8A</p>
+					<p>Maths 8A</p>
+				</td>
+				<td>First Semester</td>
+				<td>
+					<p>22-01-2011</p>
+					<p>10AM to 12PM</p>
+					<p>Room 404</p>
+					<p>Old Building</p>
+				</td>
+				<td>
+					<div class="toggleButton">
+						<a href="#" class="l on">On</a>
+						<a href="#" class="l">Off</a>
+						<span class="clear"></span>
+					</div>
+					
+				</td>
+				<td>
+					<p><a href="#">View</a>/ <a href="#">Edit</a></p>
+					<p><a href="#">Enter result</a></p>
+					<p><a class="tRed" href="#">Delete</a></p>
+				</td>
+			</tr>
+			<tr>
+				<td><input type="checkbox" /></td>
+				<td>Maths M01</td>
+				<td>
+					<p>Standard 8A</p>
+					<p>Maths 8A</p>
+				</td>
+				<td>First Semester</td>
+				<td>
+					<p>22-01-2011</p>
+					<p>10AM to 12PM</p>
+					<p>Room 404</p>
+					<p>Old Building</p>
+				</td>
+				<td>
+					<div class="toggleButton">
+						<a href="#" class="l on">On</a>
+						<a href="#" class="l">Off</a>
+						<span class="clear"></span>
+					</div>
+					
+				</td>
+				<td>
+					<p><a href="#">View</a>/ <a href="#">Edit</a></p>
+					<p><a href="#">Enter result</a></p>
+					<p><a class="tRed" href="#">Delete</a></p>
+				</td>
+			</tr>
+			<tr>
+				<td><input type="checkbox" /></td>
+				<td>Maths M01</td>
+				<td>
+					<p>Standard 8A</p>
+					<p>Maths 8A</p>
+				</td>
+				<td>First Semester</td>
+				<td>
+					<p>22-01-2011</p>
+					<p>10AM to 12PM</p>
+					<p>Room 404</p>
+					<p>Old Building</p>
+				</td>
+				<td>
+					<div class="toggleButton">
+						<a href="#" class="l on">On</a>
+						<a href="#" class="l">Off</a>
+						<span class="clear"></span>
+					</div>
+					
+				</td>
+				<td>
+					<p><a href="#">View</a>/ <a href="#">Edit</a></p>
+					<p><a href="#">Enter result</a></p>
+					<p><a class="tRed" href="#">Delete</a></p>
+				</td>
+			</tr>
+			<tr>
+				<td><input type="checkbox" /></td>
+				<td>Maths M01</td>
+				<td>
+					<p>Standard 8A</p>
+					<p>Maths 8A</p>
+				</td>
+				<td>First Semester</td>
+				<td>
+					<p>22-01-2011</p>
+					<p>10AM to 12PM</p>
+					<p>Room 404</p>
+					<p>Old Building</p>
+				</td>
+				<td>
+					<div class="toggleButton">
+						<a href="#" class="l on">On</a>
+						<a href="#" class="l">Off</a>
+						<span class="clear"></span>
+					</div>
+				</td>
+				<td>
+					<p><a href="#">View</a>/ <a href="#">Edit</a></p>
+					<p><a href="#">Enter result</a></p>
+					<p><a class="tRed" href="#">Delete</a></p>
+				</td>
+			</tr>
+			<tr class="pagination">
+				<td class="tar pagination" colspan="7">
+					<a href="#">&laquo;</a>
+					<a href="#">1</a>
+					<a href="#" class="selected">2</a>
+					<a href="#">3</a>
+					<a href="#">&raquo;</a>
+				</td>
+			</tr>
+		</table>		
+	</div><!-- pagecontent -->
+	
+	<div class="clear"></div>
+	
+
+<?php include_once 'footer.php'; ?>
