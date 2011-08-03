72aefa6d517d60356ae20dba9402bf3411fc61c3
Vineet Naik <vineet.naik@kodeplay.com>
2011-08-03 18:06:55 +0530

    add upload view for examresult

---
+++ modules/exam/views/examresult/upload.php
@@ -0,0 +1,8 @@
+<div class="r pagecontent">
+    <div class="pageTop">
+        <div class="pageTitle l">Exam Results</div>
+        <div class="pageDesc r">this is a test description this is a test description this is a test description this is a test description this is a test description </div>
+        <div class="clear"></div>
+    </div><!-- pageTop -->
+    
+</div>
