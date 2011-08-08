83d5b565457d06cbfe09d5441ceb9584e4b43dab
Eric Fargose <eric@kodeplay.com>
2011-08-08 20:14:13 +0530

    for doing the marksheet complete

--- modules/exam/views/examresult/exammarksheet.php
+++ modules/exam/views/examresult/exammarksheet.php
@@ -13,30 +13,88 @@
                  <th>Total Marks</th>
                  <th>Passing Marks</th>
             </tr>
+             <?php 
+            $marks_total = 0;
+            $total_marks_total = 0;
+            $passing_marks_total = 0; 
+            ?>
             <?php foreach($marksheets as $marksheet){ ?>
+           
             <tr>
                 <td>
                     <?php
                         echo $name = $marksheet->name; 
                     ?>
                 </td>
+                
+                <?php 
+                    $marks = $marksheet->marks;
+                    $marks_total = $marks_total + $marks;
+                    if($marks < $marksheet->passing_marks)
+                    {
+                ?>
+                    <td class="tRed">
+                        <?php 
+                            echo $marks;
+                        ?>
+                    </td>
+                <?php 
+                    } else {
+                ?>
+                    <td>
+                        <?php 
+                            echo $marks;
+                        ?>
+                    </td>
+                <?php 
+                    }
+                ?>
                 <td>
-                    <?php echo $marksheet->marks; ?>
+                    <?php 
+                        $total_marks = $marksheet->total_marks; 
+                        echo $total_marks; 
+                        $total_marks_total = $total_marks_total + $total_marks;
+                    ?>
                 </td>
                 <td>
-                    <?php echo $marksheet->total_marks; ?>
-                </td>
-                <td>
-                    <?php echo $marksheet->passing_marks; ?>
+                    <?php 
+                        $passing_marks = $marksheet->passing_marks; 
+                        echo $passing_marks; 
+                        $passing_marks_total = $passing_marks_total + $passing_marks;
+                    ?>
                 </td>
                  
             </tr>    
             <?php 
             }
             ?>
-            
+            <tr>
+                <td>
+                    Total
+                </td>
+                <td>
+                    <?php echo $marks_total; ?>
+                </td>
+                <td>
+                    <?php echo $total_marks_total; ?>
+                </td>
+                <td>
+                    <?php echo $passing_marks_total; ?>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Percentage
+                </td>
+                <td colspan="3">
+                    <?php 
+                        $percentage = ($marks_total * 100)/$total_marks_total;
+                        echo $percentage;
+                    ?>%
+                </td>
+            </tr>
         </table>
     </div><!-- pagecontent -->
     
 <div class="clear"></div>
-</div>
+
