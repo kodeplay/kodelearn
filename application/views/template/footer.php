aa505850c2a7a67f1b82956b4cda4dc49b9d3087
Vineet Naik <vineet.naik@kodeplay.com>
2011-08-17 17:04:29 +0530

    modify footer.php file to try out i18n function in views

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,7 +1,7 @@
 		</div><!-- container -->
 		
 		<div class="footer clear tac vpad40 tBrown">
-			This is the footer
+                    <?php echo __('This is the footer'); ?>
 		</div><!-- footer -->
 		
 	</body>
