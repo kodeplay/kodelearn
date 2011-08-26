8066e9ca8dfd21dec4fae1d551a4f6f1de4e39da
Jimit Modi <jimit@kodeplay.com>
2011-08-26 15:12:57 +0530

    change the timezone to asia/kolkata

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -22,7 +22,7 @@ else
  * @see  http://kohanaframework.org/guide/using.configuration
  * @see  http://php.net/timezones
  */
-date_default_timezone_set('Kolkata/India');
+date_default_timezone_set('Asia/Kolkata');
 
 /**
  * Set the default locale.
