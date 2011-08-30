b10a0c1575068e10400d1860c5e7b8ad1b23b434
Eric Fargose <eric@kodeplay.com>
2011-08-30 13:04:44 +0530

    for changing class file

--- modules/exam/classes/feed/exam.php
+++ modules/exam/classes/feed/exam.php
@@ -8,10 +8,22 @@ class Feed_Exam extends Feed {
     	$this->id = $id;
     	$this->type = $feed->type;
     	$this->action = $feed->action;
+    	$this->respective_id  = $feed->respective_id ;
+    	$this->actor_id  = $feed->actor_id ;
     }
     
     public function render(){
-        return $this->type . ' ' . $this->action .' '. $this->id;
+        $exam = ORM::factory('exam', $this->respective_id);
+        $user = ORM::factory('user', $this->actor_id);
+        $event = ORM::factory('event', $exam->event_id);
+        
+        $view = View::factory('feed/'.$this->action)
+               ->bind('exam', $exam)
+               ->bind('user', $user)
+               ->bind('event', $event)
+               ;
+        return $view->render();
+        //return $this->type . ' ' . $this->action .' '. $this->id;
     }
     
 }
