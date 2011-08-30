f4d24347a6dcbc3e891179b4165c24886885937a
Jimit Modi <jimit@kodeplay.com>
2011-08-30 18:03:56 +0530

    add feed for lectures

--- modules/feed/views/feed/index.php
+++ modules/feed/views/feed/index.php
@@ -8,7 +8,9 @@
         <div id="feeds">
             <?php if($feeds) {?>
             <?php foreach($feeds as $feed_html){?>
-            <?php echo $feed_html; ?>
+	            <div class="vpad10">
+                    <?php echo $feed_html; ?>
+                </div>
             <?php }?>
             <?php }?>
         </div>
