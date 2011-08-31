6bf93aff01a32837f7668809e13a7c5bfa102a8e
Eric Fargose <eric@kodeplay.com>
2011-08-31 19:00:30 +0530

    for completing the batch and course feed part

---
+++ modules/feed/views/feed/batch_add.php
@@ -0,0 +1,19 @@
+<?php 
+    $image = CacheImage::instance();
+    $avatar = $image->resize($user->avatar, 50, 50);
+?>
+
+    
+    <table class="fullwidth">
+        <tr>
+            <td class="w8">
+                <img src = "<?php echo $avatar; ?>"></img>
+            </td>
+            <td class="vatop">
+                <p class="h3"><?php echo $user->fullname(); ?></p><br>
+                <p class="h5 lh140" >Has added you to <?php echo $batch->name; ?> batch <?php if($count_user > 1){ echo "with <span class='bold'>".$count_user."</span> students"; } ?> 
+                
+                </p>
+            </td>
+        </tr>
+    </table>
