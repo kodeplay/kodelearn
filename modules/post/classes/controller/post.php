9db249e797b4917add62d9cba4a6457ed59ba4ea
Vineet Naik <vineet.naik@kodeplay.com>
2011-09-03 13:52:44 +0530

    rename module posts to post and add blank controller

---
+++ modules/post/classes/controller/post.php
@@ -0,0 +1,20 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Post extends Controller_Base {
+
+    /**
+     * Action to display the form for adding posts
+     */
+    public function action_form() {
+
+
+    }
+
+    /**
+     * Action for adding the post to database
+     */
+    public function action_add() {
+
+    }
+
+}
