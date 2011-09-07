f86bb7ec80de06dc9e02b32e9b57ef493f574362
Eric Fargose <eric@kodeplay.com>
2011-09-07 12:52:39 +0530

    for doing the parent email thing for approval-unapproval

--- application/classes/model/user.php
+++ application/classes/model/user.php
@@ -188,16 +188,21 @@ class Model_User extends ORM {
 
     public function send_parent_email(){
         $parent = ORM::factory('user', $this->parent_user_id);
-        $forgot_password_string = md5($parent->email.time());
-        $parent->forgot_password_string = $forgot_password_string;
-        $parent->save(); 
-        
-        $subject = "Parent email";
-        $message  = "<b>Dear ". $parent->firstname ." ". $parent->lastname .",<br><br>";
-        $message .= "Your child '". $this->firstname ." ". $this->lastname ."' has registered on Kodelearn. <br>The link to access your account is ".Url::site("auth")." <br>";
-        $message .= "User name : ". $parent->email ."<br>"; 
-        $message .= "Set password first : ". Url::site("auth/changepassword/u/".$forgot_password_string); 
-
+        if($parent->status == '1'){
+            $forgot_password_string = md5($parent->email.time());
+            $parent->forgot_password_string = $forgot_password_string;
+            $parent->save(); 
+            
+            $message  = "<b>Dear ". $parent->firstname ." ". $parent->lastname .",<br><br>";
+            $message .= "Your child '". $this->firstname ." ". $this->lastname ."' has registered on Kodelearn. <br>The link to access your account is ".Url::site("auth")." <br>";
+            $message .= "User name : ". $parent->email ."<br>"; 
+            $message .= "Set password first : ". Url::site("auth/changepassword/u/".$forgot_password_string); 
+            
+        } else {
+            $message  = "<b>Dear ". $parent->firstname ." ". $parent->lastname .",<br><br>";
+            $message .= "Your account has been created on Kodelearn. <br>But it is waiting for admin approval <br>";
+        }
+        $subject = "Parent registration email";
         $message .=  "<br><br>Thanks,<br> Kodelearn team";
         $html = true;
         Email::send_mail($parent->email, $subject, $message, $html);
