5fa274155bf07fceb23357fe357496f4fe6b6fdd
Nikhil Tailang <nikhil@kodeplay.com>
2011-09-13 16:27:58 +0530

    Fixed #1456. Role checkboxes are now in line with the visibility select box in post feed form.

--- modules/post/views/post/form.php
+++ modules/post/views/post/form.php
@@ -1,36 +1,37 @@
 <div class="clear"></div>
 <div class="pad10 tm10" id="post-form">
     <form name="post_status" method="post">
-        <textarea name="post" id="post"></textarea>
+        <textarea name="post" id="post" class="l"></textarea>
         <a class="button r">Post</a>
         <div class="clear"></div>
-        <div class="vpad10 tm10 hgtfix">
+        <div class="vpad10 hgtfix">
             Share with: &nbsp;
-            <select name="post_setting">
+            <select name="post_setting" class="tm10">
                 <?php foreach ($visibility_options as $key=>$option) { ?>
                     <option value="<?php echo $key; ?>"><?php echo $option; ?></option>
                 <?php } ?>
             </select>&nbsp;
             <?php if ($courses) { ?>
-            <select class="hidden" name="course">
+            <select class="tm10 hidden" name="course">
                 <?php foreach ($courses as $course_id=>$course_name) { ?>
                 <option value="<?php echo $course_id; ?>"><?php echo $course_name; ?></option>
                 <?php } ?>
             </select>
             <?php } ?>
             <?php if ($batches) { ?>
-            <select class="hidden" name="batch">
+            <select class="tm10 hidden" name="batch">
                 <?php foreach ($batches as $batch_id=>$batch_name) { ?>
                 <option value="<?php echo $batch_id; ?>"><?php echo $batch_name; ?></option>
                 <?php } ?>
             </select>
             <?php } ?>
-            <div class="clear"></div>
-            <ul class="hidden tm10">
+            
+            <ul class="hidden tm10 dib vabot">
                 <?php foreach ($roles as $role_id=>$role_name) { ?>
                     <li><input type="checkbox" name="selected_roles[]" value="<?php echo $role_id; ?>" /><label><?php echo $role_name; ?></label></li>
                 <?php } ?>
             </ul>            
+			<div class="clear"></div>
         </div>
     </form>     
 </div>
