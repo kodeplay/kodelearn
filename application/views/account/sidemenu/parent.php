fb38780c694e59db7d24ef272ed97752e4011c8b
Vinod Puliyadi <vinodp@kodeplay.com>
2011-09-20 15:34:29 +0530

    change the css class name of hover link on profile image

--- application/views/account/sidemenu/parent.php
+++ application/views/account/sidemenu/parent.php
@@ -1,5 +1,5 @@
 <?php if (isset($avatar_user)) { ?>
-    <div class="tac" id="profile_image"><div class="prel"><a href="<?php echo $change_img_url; ?>" class="profileimg_link">Change</a><img id="sbProfileImg" src="<?php echo $avatar_user; ?>" /></div></div>
+    <div class="tac hlcontainer" id="profile_image"><div class="prel"><p class="hoverLinks"><a href="<?php echo $change_img_url; ?>" >Change</a></p><img id="sbProfileImg" src="<?php echo $avatar_user; ?>" /></div></div>
     <p class="tac sidebarTitle" id="sbName"><?php echo $user->firstname . ' ' . $user->lastname; ?></p>
     <p class="tac h4" id="sbType"><?php echo $role; ?></p>
     <p align="center">
