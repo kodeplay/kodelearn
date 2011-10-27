666c5cf44c3430ea4079b95ab1baa92b796d19ad
Vineet Naik <vineet.naik@kodeplay.com>
2011-10-27 16:28:57 +0530

    fix course_id not getting stored in feed table

--- modules/feed/classes/model/feed.php
+++ modules/feed/classes/model/feed.php
@@ -8,59 +8,59 @@ class Model_Feed extends ORM {
             'through' => 'feeds_users',
         ),
     );
-          
-	/*
-	 * This will see for the role of the user and depending on that all the feeds of the user will be loaded
-	 * 
-	 * @return array of feed object
-	 */
-	public static function get_feeds($data = array()){
-		
-		$data = array_merge(array(
-		 'limit'     => 5,
-		 'offset'    => 0
-		) , $data);
-		
-		$user = Acl::instance()->relevant_user();
-		
-		if(!$user)
-		  $user = Auth::instance()->get_user();
-		  
-		$feed = ORM::factory('feed')
-		        ->join('feeds_users')
-		        ->on('feeds.id', '=', 'feeds_users.feed_id')
-		        ->where('feeds_users.user_id', '=', $user->id);
-		if(isset($data['course_id'])){
-		    $feed->where('course_id', '=', $data['course_id']);
-		}               
-		               
-		$feed->order_by('time', 'DESC')
-		     ->limit($data['limit'])
-		     ->offset($data['offset']);
-
-		$feeds = $feed->find_all();
-            
-	   return $feeds;
-		
-	}
-	
-	public static function get_total_feeds($data = array()){
+    
+    /*
+     * This will see for the role of the user and depending on that all the feeds of the user will be loaded
+     * 
+     * @return array of feed object
+     */
+    public static function get_feeds($data = array()){
         
-		$user = Acl::instance()->relevant_user();
+        $data = array_merge(array(
+            'limit'     => 5,
+            'offset'    => 0
+        ) , $data);
+        
+        $user = Acl::instance()->relevant_user();
         
         if(!$user)
-          $user = Auth::instance()->get_user();
-          
+            $user = Auth::instance()->get_user();
+        
         $feed = ORM::factory('feed')
-                       ->join('feeds_users')
-                       ->on('feeds.id', '=', 'feeds_users.feed_id')
-                       ->where('feeds_users.user_id', '=', $user->id);
+            ->join('feeds_users')
+            ->on('feeds.id', '=', 'feeds_users.feed_id')
+            ->where('feeds_users.user_id', '=', $user->id);
+        if(isset($data['course_id'])){
+            $feed->and_where('course_id', '=', $data['course_id']);
+        }              
+        
+        $feed->order_by('time', 'DESC')
+            ->limit($data['limit'])
+            ->offset($data['offset']);
+
+        $feeds = $feed->find_all();
+        
+        return $feeds;
+        
+    }
+    
+    public static function get_total_feeds($data = array()){
+        
+        $user = Acl::instance()->relevant_user();
+        
+        if(!$user)
+            $user = Auth::instance()->get_user();
+        
+        $feed = ORM::factory('feed')
+            ->join('feeds_users')
+            ->on('feeds.id', '=', 'feeds_users.feed_id')
+            ->where('feeds_users.user_id', '=', $user->id);
         if(isset($data['course_id'])){
             $feed->where('course_id', '=', $data['course_id']);
         } 
         $feeds = $feed->count_all();
-               
-       return $feeds;
-		
-	} 
+        
+        return $feeds;
+        
+    } 
 }
