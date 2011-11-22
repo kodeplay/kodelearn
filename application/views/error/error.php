d198b134e0f3c6e69aea1ac605b029d6cb13d8b5
Vineet Naik <vineet.naik@kodeplay.com>
2011-11-22 20:53:35 +0530

    fix ui for error page

--- application/views/error/error.php
+++ application/views/error/error.php
@@ -1,6 +1,6 @@
 <div class="r pagecontent">
     <div class="pageTop withBorder">
-        <div class="pageTitle l w60"><?php echo $heading; ?></div>
+        <div class="pageTitle l w90"><?php echo $heading; ?></div>
         <div class="clear"></div>
     </div><!-- pageTop -->    
 </div><!-- content -->
